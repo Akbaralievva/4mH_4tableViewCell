bf2c3dd4c31488ebc2f8dc47ba51510465dc917d
Abduraimov <abduraim0vnurtilek26@gmail.com>
2024-02-24 02:00:49 +0600

    Initial Commit

---
+++ 4monthHomeWork4tableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4monthHomeWork4tableViewCell
+//
+//  Created by Nurtilek on 2/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
